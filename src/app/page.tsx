54d6f6766ca17a7c96625a4dedd34e66af039baf
Baek <woonn97@gmail.com>
2024-08-15 00:21:24 +0900

    [🔨chore]: prettier 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main>
       <div>
-        <span> hi</span>
+        <span>hi</span>
       </div>
     </main>
   );
