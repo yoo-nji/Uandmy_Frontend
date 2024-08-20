2871829d60e0f1fc41b3b159fc4e6f85823c960b
jioneee <qoxx3659@gmail.com>
2024-08-20 10:44:58 +0900

    [✨feat]: CheckBox 컴포넌트 구현

--- src/components/common/CheckBox.tsx
+++ src/components/common/CheckBox.tsx
@@ -8,22 +8,28 @@ type LabelProps = {
 
 const CheckBox = ({ label }: LabelProps) => {
   const [isChecked, setIsChecked] = useState(false);
+
   return (
     <div className="flex items-center">
-      <input
-        id={`checkbox-${label}`}
-        type="checkbox"
-        checked={isChecked}
-        onChange={() => setIsChecked(!isChecked)}
-        className="w-4 h-4 appearance-none cursor-pointer rounded-lg peer border-[#D9D9D9]  checked:border-[#6224FD] "
-      />
       <label
         htmlFor={`checkbox-${label}`}
-        className="flex items-center  w-auto h-[2.5rem] cursor-pointer box-border border-2 border-[#D9D9D9] rounded-lg px-2 checked:border-[#6224FD] checked:text-[#6224FD">
+        className={`flex items-center w-auto h-[2.5rem] cursor-pointer box-border border-2 rounded-lg pl-1 pr-2 ${
+          isChecked ? 'bg-[#EFE9FF] border-primary' : 'border-[#D9D9D9]'
+        }`}>
+        <input
+          id={`checkbox-${label}`}
+          type="checkbox"
+          checked={isChecked}
+          onChange={() => setIsChecked(!isChecked)}
+          className="appearance-none w-4 h-4 rounded-lg peer -ml-2 focus:outline-none focus:ring-0 focus:border-transparent "
+        />
         <CheckSvg fillColor={isChecked ? '#6224FD' : '#E9E9E9'} />
-        <span className="ml-1">{label}</span>
+        <span className={`ml-2 ${isChecked ? 'text-primary' : 'text-black'}`}>
+          {label}
+        </span>
       </label>
     </div>
   );
 };
+
 export default CheckBox;
