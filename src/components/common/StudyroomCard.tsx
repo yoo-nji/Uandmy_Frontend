ea94a7f544b392267ca37bd56841572e9832e8aa
Hyerim Yang <hlhlstar@naver.com>
2024-08-21 18:50:23 +0900

    [✨feat]: StudyroomCard 컴포넌트에 props 전달 및 렌더링
    - StudyroomCard 컴포넌트가 position, title, tags, startDate, endDate, views와 같은 props를 받도록 수정.
    
    - startDate, endDate 날짜 포맷 설정 및 D-day 계산 로직 구현.
    - endDate 연도에 따른 날짜 포맷 조건부 렌더링 구현
    - startDate와 endDate가 동일한 연도일 경우, 연도를 한 번만 표시하도록 조건부 렌더링 로직 추가
    - 연도가 다를 경우, startDate와 endDate 각각의 연도를 모두 표시
    
    - 태그를 동적으로 렌더링하기 위한 TagBox 컴포넌트 생성.

--- src/components/common/StudyroomCard.tsx
+++ src/components/common/StudyroomCard.tsx
@@ -2,34 +2,66 @@ import React from 'react';
 import Bookmark from '../../../public/images/bookmark.svg';
 import Calendar from '../../../public/images/event_available.svg';
 import View from '../../../public/images/Eye.svg';
+import TagBox from './TagBox';
 
-const StudyroomCard = () => {
+interface StudyroomCardProps {
+  position: string;
+  title: string;
+  tags: string[];
+  startDate: string; // "YYYY-MM-DD" 또는 "YYYY/MM/DD" 형식으로 입력 (ISO 8601 형식)
+  endDate: string; // "YYYY-MM-DD" 또는 "YYYY/MM/DD" 형식으로 입력 (ISO 8601 형식)
+  views: number;
+}
+
+const StudyroomCard = ({
+  position,
+  title,
+  tags,
+  startDate,
+  endDate,
+  views,
+}: StudyroomCardProps) => {
+  // startDate, endDate 문자열을 Date 객체로 변환
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+
+  // 요일
+  const dayOfTheWeek = ['일', '월', '화', '수', '목', '금', '토'];
+
+  // D-day
+  const diffInMs = end.getTime() - start.getTime();
+  const diffInDays = Math.ceil(diffInMs / (1000 * 60 * 60 * 24));
+
   return (
     <div className="w-[21.4375rem] h-[10.5625rem] px-[1rem] py-[1.125rem] rounded-lg border-[.0625rem] border-[#EAEAEA] drop-shadow-custom">
       <div className="flex justify-between items-center mb-[.5rem]">
-        <div className="text-[#555555] text-xs">모집 직군</div>
+        <div className="text-[#555555] text-xs">{position}</div>
         <Bookmark />
       </div>
 
-      <p className="text-base text-[#434343] font-semibold">제목</p>
+      <p className="text-base text-[#434343] font-semibold">{title}</p>
 
       <div className="flex flex-wrap gap-2 mt-[.75rem] mb-[1.5rem]">
-        <div>tag</div>
-        <div>tag</div>
-        <div>tag</div>
+        {tags.map((tag, idx) => (
+          <TagBox key={idx} label={tag} />
+        ))}
       </div>
 
       <div className="flex justify-between items-center text-xs">
         <div className="flex gap-x-3">
-          <div className="font-bold text-primary">D-day</div>
+          <div className="font-bold text-primary">D-{diffInDays}</div>
           <div className="text-[#BEB4BF] flex items-center gap-1">
             <Calendar />
-            <p>yyyy.mm.dd (요일) - mm.dd (요일)</p>
+            {start.getFullYear() === end.getFullYear() ? (
+              <p>{`${start.getFullYear()}.${start.getMonth() + 1}.${start.getDate()} (${dayOfTheWeek[start.getDay()]}) - ${end.getMonth() + 1}.${end.getDate()} (${dayOfTheWeek[end.getDay()]})`}</p>
+            ) : (
+              <p>{`${start.getFullYear()}.${start.getMonth() + 1}.${start.getDate()} (${dayOfTheWeek[start.getDay()]}) - ${end.getFullYear()}.${end.getMonth() + 1}.${end.getDate()} (${dayOfTheWeek[end.getDay()]})`}</p>
+            )}
           </div>
         </div>
         <div className="text-[#908794] flex items-center gap-[.1875rem]">
           <View />
-          <p>조회수</p>
+          <p>{views}</p>
         </div>
       </div>
     </div>
