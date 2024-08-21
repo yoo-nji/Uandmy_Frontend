bd70927965e4c9f3daf1104854362d37409f316d
jioneee <qoxx3659@gmail.com>
2024-08-21 11:17:01 +0900

    [✨feat]: Input 컴포넌트 구현

---
+++ src/components/common/Input.tsx
@@ -0,0 +1,20 @@
+'use client';
+import { InputHTMLAttributes } from 'react';
+type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+  placeholder?: string;
+  className?: string;
+};
+
+const Input = ({ placeholder, type, className, ...rest }: InputProps) => {
+  return (
+    <label>
+      <input
+        {...rest}
+        type={type}
+        placeholder={placeholder}
+        className={`w-[21.438rem] h-[3.125rem] border-solid border-2 border-[#CED4DA] rounded-lg placeholder-[#82829B] p-[0.625rem] ${className}`}
+      />
+    </label>
+  );
+};
+export default Input;
