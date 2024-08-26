a78abb41bebec3c6136e8e81b531630bb5dc31ce
Hyerim Yang <hlhlstar@naver.com>
2024-08-26 20:34:47 +0900

    [✨feat]: 스터디 탐색하기 페이지 퍼블리싱 (슬라이드 메뉴, 필터바 제외)

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,193 @@
-import React from 'react';
+'use client';
+import { useState } from 'react';
+import Gnb from '@/components/common/Gnb';
+import StudyroomCard, {
+  StudyroomCardProps,
+} from '@/components/common/StudyroomCard';
+import MagnifyingGlass from '../../../public/images/MagnifyingGlass.svg';
+import ToggleSwitch from '@/components/common/ToggleSwitch';
+
+const searchResults: StudyroomCardProps[] = [
+  {
+    position: '개발',
+    title: '자바 중급 스터디 모집',
+    tags: ['북 스터디', 'Java', '백엔드 개발자'],
+    startDate: '2024/05/29',
+    endDate: '2024/06/29',
+    views: 8123456,
+  },
+  {
+    position: '디자이너 | UXUI 디자인',
+    title: '피그마 고급 스킬 스터디 모집 🥰',
+    tags: ['오토레이아웃', '과제인증 필수'],
+    startDate: '2024/09/16',
+    endDate: '2024/10/10',
+    views: 1203,
+  },
+  {
+    position: '디자이너 | 그래픽 디자인',
+    title: '하반기 영상 공모전 대비 스터디',
+    tags: ['C4D', '블렌더', '3D 디자인'],
+    startDate: '2024/09/20',
+    endDate: '2024/10/20',
+    views: 823,
+  },
+];
+
+const recommendedStudyroomCards: StudyroomCardProps[] = [
+  {
+    position: '디자이너 | UXUI 디자인',
+    title: '피그마 고급 스킬 스터디 모집 🥰',
+    tags: ['오토레이아웃', '과제인증 필수'],
+    startDate: '2024/09/16',
+    endDate: '2024/10/10',
+    views: 1203,
+  },
+  {
+    position: '디자이너 | 그래픽 디자인',
+    title: '하반기 영상 공모전 대비 스터디',
+    tags: ['C4D', '블렌더', '3D 디자인'],
+    startDate: '2024/09/20',
+    endDate: '2024/10/20',
+    views: 823,
+  },
+];
 
 const Page = () => {
-  return <div>탐색</div>;
+  const [showRecruitingOnly, setShowRecruitingOnly] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setShowRecruitingOnly(!showRecruitingOnly);
+  };
+
+  // 모집 중인 카드 항목 필터링
+  const today = new Date();
+  const filteredStudyroomCardItems = searchResults.filter((cardItem) => {
+    return showRecruitingOnly
+      ? new Date(cardItem.startDate).getTime() >= today.getTime()
+      : true;
+  });
+
+  return (
+    <div className="w-[23.4375rem] min-h-screen bg-[#F6F6F6]">
+      <div className="w-full px-[.9375rem] pt-11 pb-6 bg-white">
+        {/* Contents */}
+        <header className="h-10 flex items-center mb-[.6875rem] text-lg text-[#212529] font-bold">
+          탐색하기
+        </header>
+
+        <div className="relative">
+          <span className="absolute inset-y-0 left-0 flex items-center pl-[14px]">
+            <MagnifyingGlass />
+          </span>
+          <input
+            type="text"
+            name="search"
+            className="block w-full pl-11 pr-[.875rem] py-[.6875rem] rounded-lg bg-[#F3F3F3] placeholder:text-[#41364A] text-[#41364A] text-sm ring-1 ring-inset ring-[#DDDDDD] focus:ring-2 focus:ring-[#DDDDDD] focus:outline-none"
+            placeholder="어떤 스터디를 찾고 싶나요?"
+            // value={inputValue}
+            // onChange={handleInputChange}
+          />
+        </div>
+
+        <div className="mt-2 mb-[1.5625rem] h-6 flex justify-end items-center gap-x-1.5">
+          <p className="text-[.8125rem]">원하는 스터디 알림 받기</p>
+          <ToggleSwitch />
+        </div>
+
+        {/* 스터디 찾기 | 팀원 찾기 */}
+        {/* 태그 */}
+        {/* 필터 */}
+        {/* 총 개수, 최신 순, 등록일 전체 */}
+      </div>
+
+      {/* 검색 및 추천 결과 */}
+      <div className="w-full">
+        {/* 검색 결과 */}
+        <div className="px-[.9375rem] pb-[3.375rem] border-t-8 border-[#F2F2F2]">
+          <div className="my-[1.125rem] flex justify-between">
+            <label className="flex items-center text-sm text-[#262626]">
+              <input
+                type="checkbox"
+                checked={showRecruitingOnly}
+                onChange={handleCheckboxChange}
+                className="hidden"
+              />
+              <svg
+                width="18"
+                height="18"
+                viewBox="0 0 18 18"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="mr-[.5625rem] rounded">
+                <rect
+                  width="18"
+                  height="18"
+                  rx="4"
+                  fill={showRecruitingOnly ? '#837486' : 'none'}
+                  stroke="#837486"
+                  strokeWidth="2"
+                />
+                <path
+                  d="M14.3337 5L7.00033 12.3333L3.66699 9"
+                  stroke="#FBFBFB"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+              모집 중인 스터디만 보기
+            </label>
+            {/* <p className="text-sm"> 2 / 6</p> */}
+          </div>
+
+          <div className="flex flex-col gap-y-[1.125rem]">
+            {filteredStudyroomCardItems.map((cardItem, idx) => (
+              <StudyroomCard
+                key={idx}
+                position={cardItem.position}
+                title={cardItem.title}
+                tags={cardItem.tags}
+                startDate={cardItem.startDate}
+                endDate={cardItem.endDate}
+                views={cardItem.views}
+              />
+            ))}
+          </div>
+        </div>
+
+        {/* 추천 결과 */}
+        <div className="px-[.9375rem] pb-20 border-t-8 border-[#F2F2F2] bg-white">
+          <div className="flex justify-between mt-[1.375rem] mb-4">
+            <p className="text-lg text-[#212529] font-semibold">
+              서희님과 비슷한 사용자가
+              <br />
+              방금 지원했어요
+            </p>
+            <div className="flex flex-col justify-end">
+              {/* <p className="text-sm"> 2 / 6</p> */}
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-y-[1.125rem]">
+            {recommendedStudyroomCards.map((cardItem, idx) => (
+              <StudyroomCard
+                key={idx}
+                position={cardItem.position}
+                title={cardItem.title}
+                tags={cardItem.tags}
+                startDate={cardItem.startDate}
+                endDate={cardItem.endDate}
+                views={cardItem.views}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <Gnb />
+    </div>
+  );
 };
 
 export default Page;
