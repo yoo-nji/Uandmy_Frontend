c2131820a6370eb9be46b667656f3a52b4fdcd9b
jioneee <qoxx3659@gmail.com>
2024-08-28 01:23:47 +0900

    [✨feat]: JobCard 컴포넌트 구현

--- src/components/onboarding/JobCard.tsx
+++ src/components/onboarding/JobCard.tsx
@@ -1,7 +1,30 @@
-const JobCard = () => {
+'use client';
+import Image, { StaticImageData } from 'next/image';
+
+interface JobPops {
+  id: string;
+  label: string;
+  img: StaticImageData;
+  isSelected: boolean;
+  onClick: () => void;
+}
+
+const JobCard = ({ id, label, img, isSelected, onClick }: JobPops) => {
   return (
     <>
-      <h1>JobCard Component</h1>
+      <div
+        key={id}
+        className={`box-border w-[6.813rem] h-[7.5rem] ${
+          isSelected
+            ? 'border-2 border-primary bg-[#EFE9FF]'
+            : 'border-2 border-none  bg-[#F5F5F5]'
+        } flex justify-center items-center flex-col rounded-lg space-y-2`}
+        onClick={onClick}>
+        <Image src={img} alt={label} className="w-[1.5rem] h-[1.5rem]" />
+        <p className={`${isSelected ? 'text-primary' : 'text-black'} `}>
+          {label}
+        </p>
+      </div>
     </>
   );
 };
