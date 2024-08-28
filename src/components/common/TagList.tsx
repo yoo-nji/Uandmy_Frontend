aefc48ab7464bc82973447b55a0309b655e90708
Baek <woonn97@gmail.com>
2024-08-28 21:53:34 +0900

    [📝docs]: jsDoc 작성

--- src/components/common/TagList.tsx
+++ src/components/common/TagList.tsx
@@ -10,6 +10,17 @@ interface TagListProps {
   className?: string;
 }
 
+/**
+ * 태그 목록을 표시하는 컴포넌트입니다.
+ *
+ * @param {TagListProps} props - 컴포넌트의 속성 (Props).
+ * @param {string[]} props.tagData - 표시할 태그 문자열 배열.
+ * @param {(selectedTag: string[]) => void} [props.onTagSelect] - 선택된 태그가 변경될 때 호출되는 선택적 콜백 함수.
+ * @param {string} [props.className] - 선택적 CSS 클래스 이름.
+ *
+ * @returns {JSX.Element} 렌더링된 TagList 컴포넌트.
+ */
+
 const TagList = ({ tagData, onTagSelect, className }: TagListProps) => {
   const [selectedTags, setSelectedTags] = useState<string[]>([]);
   const handleTagClick = (label: string) => {
