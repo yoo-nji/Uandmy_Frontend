7676cc7299a2e5429ce244f329c32a0ba02f8530
Baek <woonn97@gmail.com>
2024-08-28 21:52:32 +0900

    [♻️refactor]: TagList 리팩토링

--- src/components/common/TagList.tsx
+++ src/components/common/TagList.tsx
@@ -4,25 +4,28 @@ import { useState } from 'react';
 
 import Tag from './Tag';
 
-const tagData = [
-  '주도적인',
-  '열정적인',
-  '손이 빠른',
-  '신뢰감',
-  '노력가',
-  '팀워크형',
-];
-const TagList = () => {
+interface TagListProps {
+  tagData: string[];
+  onTagSelect?: (selectedTag: string[]) => void;
+  className?: string;
+}
+
+const TagList = ({ tagData, onTagSelect, className }: TagListProps) => {
   const [selectedTags, setSelectedTags] = useState<string[]>([]);
   const handleTagClick = (label: string) => {
-    setSelectedTags((prev) =>
-      prev.includes(label)
-        ? prev.filter((tag) => tag !== label)
-        : [...prev, label],
-    );
+    const updatedTags = selectedTags.includes(label)
+      ? selectedTags.filter((tag) => tag !== label)
+      : [...selectedTags, label];
+
+    setSelectedTags(updatedTags);
+
+    // 선택된 태그가 변경될 때 부모 컴포넌트로 변경 사항을 알림
+    if (onTagSelect) {
+      onTagSelect(updatedTags);
+    }
   };
   return (
-    <div className="flex flex-wrap">
+    <div className={className}>
       {tagData.map((tag) => (
         <Tag
           key={tag}
