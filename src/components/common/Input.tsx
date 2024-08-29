89993a4741b8cc11c71df6f5aa63fc3d3194b202
Baek <woonn97@gmail.com>
2024-08-29 22:30:39 +0900

    [♻️refactor]: Input 컴포넌트 조금 수정

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,12 +1,12 @@
 'use client';
-import { InputHTMLAttributes, forwardRef } from 'react';
+import { forwardRef, InputHTMLAttributes } from 'react';
 type InputProps = InputHTMLAttributes<HTMLInputElement> & {
   placeholder?: string;
   className?: string;
   type?: string;
 };
 
-const Input = forwardRef<HTMLInputElement, InputProps>(
+export const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ placeholder, type = 'text', className, ...rest }, ref) => {
     return (
       <label>
@@ -15,7 +15,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
           type={type}
           placeholder={placeholder}
           ref={ref}
-          className={`w-[21.438rem] h-[3.125rem] border-solid border-2 border-[#CED4DA] rounded-lg placeholder-[#82829B] p-[0.625rem] ${className}`}
+          className={`w-[25rem] h-[3rem] border-solid border-2 border-[#CED4DA] rounded-lg placeholder-[#82829B] p-[0.625rem] ${className}`}
         />
       </label>
     );
@@ -23,4 +23,3 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
 );
 
 Input.displayName = 'Input';
-export default Input;
