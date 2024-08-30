953ce9237e0e3aa829cce318a1b6a910f608a80c
Baek <woonn97@gmail.com>
2024-08-30 13:05:51 +0900

    [🐛fix]: window not defined 에러 해결

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 import { useFunnel } from '@use-funnel/browser';
-import SelectedJob from '@/components/onboarding/SelectedJob';
-import StudyPurpose from '@/components/onboarding/StudyPurpose';
-import StudyPeriod from '@/components/onboarding/StudyPeriod';
-import StepCompleted from '@/components/onboarding/StepCompleted';
+import React, { useEffect, useState } from 'react';
+
 import Characters from '@/components/onboarding/Characters';
+import SelectedJob from '@/components/onboarding/SelectedJob';
+import StepCompleted from '@/components/onboarding/StepCompleted';
+import StudyPeriod from '@/components/onboarding/StudyPeriod';
+import StudyPurpose from '@/components/onboarding/StudyPurpose';
 
 type OnboardingDatas = {
   selectedJob?: string;
@@ -14,66 +16,79 @@ type OnboardingDatas = {
 };
 
 const Onboarding = () => {
-  const funnel = useFunnel<{
-    selectedJob: OnboardingDatas;
-    studyPurpose: OnboardingDatas;
-    character: OnboardingDatas;
-    studyPeriod: OnboardingDatas;
-    stepCompleted: OnboardingDatas;
-  }>({
-    id: 'onboarding-funnel',
-    initial: {
-      step: 'selectedJob',
-      context: {},
-    },
-  });
+  const [isMounted, setIsMounted] = useState(false);
 
-  return (
-    <funnel.Render
-      selectedJob={({ history }) => (
-        <SelectedJob
-          onNext={(job) => {
-            console.log('job', job);
-            history.push('studyPurpose', { selectedJob: job });
-          }}
-        />
-      )}
-      studyPurpose={({ context, history }) => (
-        <StudyPurpose
-          onNext={(purposes) => {
-            console.log('purposes', purposes);
-            history.push('character', { ...context, studyPurpose: purposes });
-          }}
-          onBack={() => history.push('selectedJob', context)}
-        />
-      )}
-      character={({ context, history }) => (
-        <Characters
-          onNext={(character) => {
-            console.log('character', character);
-            history.push('studyPeriod', { ...context, character });
-          }}
-          onBack={() => history.push('studyPurpose', context)}
-        />
-      )}
-      studyPeriod={({ context, history }) => (
-        <StudyPeriod
-          onNext={(period) => {
-            console.log('period', period);
-            history.push('stepCompleted', { ...context, studyPeriod: period });
-            console.log('all', context);
-          }}
-          onBack={() => history.push('character', context)}
-        />
-      )}
-      stepCompleted={({ context }) => (
-        <StepCompleted
-          role={context.selectedJob}
-          description={context.character}
-        />
-      )}
-    />
-  );
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const FunnelComponent = () => {
+    const funnel = useFunnel<{
+      selectedJob: OnboardingDatas;
+      studyPurpose: OnboardingDatas;
+      character: OnboardingDatas;
+      studyPeriod: OnboardingDatas;
+      stepCompleted: OnboardingDatas;
+    }>({
+      id: 'onboarding-funnel',
+      initial: {
+        step: 'selectedJob',
+        context: {},
+      },
+    });
+
+    return (
+      <funnel.Render
+        selectedJob={({ history }) => (
+          <SelectedJob
+            onNext={(job) => {
+              console.log('job', job);
+              history.push('studyPurpose', { selectedJob: job });
+            }}
+          />
+        )}
+        studyPurpose={({ context, history }) => (
+          <StudyPurpose
+            onNext={(purposes) => {
+              console.log('purposes', purposes);
+              history.push('character', { ...context, studyPurpose: purposes });
+            }}
+            onBack={() => history.push('selectedJob', context)}
+          />
+        )}
+        character={({ context, history }) => (
+          <Characters
+            onNext={(character) => {
+              console.log('character', character);
+              history.push('studyPeriod', { ...context, character });
+            }}
+            onBack={() => history.push('studyPurpose', context)}
+          />
+        )}
+        studyPeriod={({ context, history }) => (
+          <StudyPeriod
+            onNext={(period) => {
+              console.log('period', period);
+              history.push('stepCompleted', {
+                ...context,
+                studyPeriod: period,
+              });
+              console.log('all', context);
+            }}
+            onBack={() => history.push('character', context)}
+          />
+        )}
+        stepCompleted={({ context }) => (
+          <StepCompleted
+            role={context.selectedJob}
+            description={context.character}
+          />
+        )}
+      />
+    );
+  };
+
+  return isMounted ? <FunnelComponent /> : null;
 };
 
 export default Onboarding;
