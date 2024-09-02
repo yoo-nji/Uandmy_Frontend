db20cfbc32b1284bfd27edbf532e025eaeb4bc9c
jioneee <qoxx3659@gmail.com>
2024-09-02 17:30:42 +0900

    [✨feat]: 공개용 프로필 페이지 구현

--- src/app/public-profile/page.tsx
+++ src/app/public-profile/page.tsx
@@ -5,8 +5,6 @@ import Button from '@/components/common/Button';
 import StudyExperienceCard from '@/components/common/StudyExperienceCard';
 import TagList from '@/components/common/TagList';
 
-const handleAccept = () => {};
-
 const PublicProfile = () => {
   const StudyExperienceDatas = [
     {
@@ -45,16 +43,42 @@ const PublicProfile = () => {
       period: '1개월 ~ 3개월',
     },
   ];
+
+  const handleLeftClick = () => {};
+  const handleAccept = () => {};
   return (
     <>
-      <div className="flex justify-center items-center h-auto">
+      <div className="flex justify-center items-center h-auto ">
         <div className="flex items-center flex-col w-[21.438rem] space-y-5">
-          <div className="text-center space-y-5 ">
-            <div className="font-bold text-[1.125rem]">오픈 프로필</div>
-            <Avartar size={100} />
+          <div className="relative text-center space-y-5 ">
+            <div className="sticky top-0 flex items-center justify-center">
+              {/* 이전 단계로 이동 */}
+              <button onClick={handleLeftClick} className="fixed left-0">
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="cursor-pointer">
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M15.5303 3.96967C15.8232 4.26256 15.8232 4.73744 15.5303 5.03033L8.56066 12L15.5303 18.9697C15.8232 19.2626 15.8232 19.7374 15.5303 20.0303C15.2374 20.3232 14.7626 20.3232 14.4697 20.0303L6.96967 12.5303C6.67678 12.2374 6.67678 11.7626 6.96967 11.4697L14.4697 3.96967C14.7626 3.67678 15.2374 3.67678 15.5303 3.96967Z"
+                    fill="black"
+                    fill-opacity="0.8"
+                  />
+                </svg>
+              </button>
+
+              <div className="font-bold text-lg mx-auto">오픈 프로필</div>
+            </div>
+            <div className="w-[6.25rem] h-[6.25rem] p-0 border-2 border-[#EADCF3] rounded-full">
+              <Avartar size={'100%'} />
+            </div>
           </div>
           <div>
-            <div className="font-semibold text-[1.25rem]">이름</div>
+            <div className="font-semibold text-[1.25rem]">제이크</div>
             <p className=" text-[0.875rem]">기획자</p>
           </div>
 
