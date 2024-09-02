7cbd4862aaafa86157bc5ec13fef1d84e55ee971
jioneee <qoxx3659@gmail.com>
2024-09-03 01:12:44 +0900

    [✨feat]: 대기중인 요청페이지에서 프로필 카드 컴포넌트 구현

---
+++ src/app/pending-request/page.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import Button from '@/components/common/Button';
+import ProfileCard from '@/components/common/ProfileCard';
+
+const PendingRequest = () => {
+  const ProfileDatas = [
+    {
+      name: '제이크',
+      job: '기획자',
+      totalStudy: 8,
+      attendance: 98,
+      text: '안녕하세요. 개발 관련 글을 꾸준히 쓰고 싶은데 의지가 부족해 스터디 버디들을 구하고 싶습니다 화이팅',
+      tags: ['손이 빠름', '열정적', '동기부여가 필요한'],
+    },
+    {
+      name: '박가현',
+      job: '디자이너',
+      totalStudy: 1,
+      attendance: 100,
+      text: '안녕하세요! 올해 졸업하고 취업 준비 중 경력도 쌓고 싶고 비슷한 사람들과 정보도 공유하고 싶어요!',
+      tags: ['취준생', '논리적인', '책임감 있는'],
+    },
+  ];
+  const handleLeftClick = () => {};
+  const handleAccept = () => {};
+
+  return (
+    <>
+      <div className="flex justify-center items-center h-auto ">
+        <div className="flex items-center flex-col w-[21.438rem] space-y-5">
+          <div className="relative text-center space-y-5 ">
+            <div className="sticky top-0 flex items-center justify-center">
+              {/* 이전 페이지로 이동 */}
+              <button onClick={handleLeftClick} className="fixed left-0">
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="cursor-pointer">
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M15.5303 3.96967C15.8232 4.26256 15.8232 4.73744 15.5303 5.03033L8.56066 12L15.5303 18.9697C15.8232 19.2626 15.8232 19.7374 15.5303 20.0303C15.2374 20.3232 14.7626 20.3232 14.4697 20.0303L6.96967 12.5303C6.67678 12.2374 6.67678 11.7626 6.96967 11.4697L14.4697 3.96967C14.7626 3.67678 15.2374 3.67678 15.5303 3.96967Z"
+                    fill="black"
+                    fill-opacity="0.8"
+                  />
+                </svg>
+              </button>
+
+              <div className="font-bold text-lg mx-auto">대기중인 요청</div>
+            </div>
+            <hr className="w-[24.125rem] border-1 border-greyBorder mt-5 mb-5" />
+          </div>
+          <ProfileCard
+            ProfileDatas={ProfileDatas}
+            handleAccept={handleAccept}
+          />
+          <div className="box-border flex flex-row gap-2 w-full ">
+            <span>
+              <p className="text-[#82829B]">수락가능인원</p>
+              <div className="flex">
+                <p className="text-primary">2명</p>
+                <p>/ 4명</p>
+              </div>
+            </span>
+
+            <Button
+              label="전체 수락하기"
+              onClick={handleAccept}
+              bgColor="bg-[#804CFF]"
+              className="w-[15rem] h-[3.063rem]"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default PendingRequest;
---
+++ src/components/common/ProfileCard.tsx
@@ -0,0 +1,70 @@
+import Avartar from './Avatar';
+import Button from './Button';
+import TagBox from './TagBox';
+
+interface ProfileCardProps {
+  ProfileDatas: {
+    name: string;
+    job: string;
+    totalStudy: number;
+    attendance: number;
+    text: string;
+    tags: string[];
+  }[];
+  handleAccept: () => void;
+}
+
+const ProfileCard = ({ ProfileDatas, handleAccept }: ProfileCardProps) => {
+  return (
+    <>
+      {ProfileDatas.map((profile, index) => (
+        <div
+          key={index}
+          className="w-full h-auto p-3 border-2 border-[#E9E9E9] rounded-lg space-y-2">
+          <div className="flex flex-row gap-2">
+            <Avartar size={56} />
+
+            <div>
+              <p className="font-semibold text-base">{profile.name}</p>
+              <p className="text-[0.813rem] text-[#65657E]">{profile.job}</p>
+              <div className="flex flex-row gap-1 text-[0.688rem] text-[#767688]">
+                <p className="">스터디 </p>
+                <p className="text-[#7876E3]">{profile.totalStudy}</p>
+                <p className="text-[#7876E3]">회</p>
+                <p>|</p>
+                <p>출석률</p>
+                <p className="text-[#7876E3]">{profile.attendance}</p>
+                <p className="text-[#7876E3]">%</p>
+              </div>
+            </div>
+
+            <div className="flex gap-1">
+              <Button
+                label={'거절'}
+                onClick={handleAccept}
+                bgColor="bg-[#F1F1F1]"
+                textColor="text-black"
+                rounded="rounded-full"
+                className="w-[3.5rem] h-[2.063rem] text-xs "
+              />
+              <Button
+                label={'수락'}
+                onClick={handleAccept}
+                rounded="rounded-full"
+                className="w-[3.5rem] h-[2.063rem] text-xs"
+              />
+            </div>
+          </div>
+
+          <p>{profile.text}</p>
+          <div className="flex flex-row gap-2">
+            {profile.tags.map((tag, index) => (
+              <TagBox key={index} label={tag} />
+            ))}
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+export default ProfileCard;
