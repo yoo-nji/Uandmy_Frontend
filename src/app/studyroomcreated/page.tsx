e450842e07a831461f6e3c31383fa965336ef780
yoonji <kyjkbw@gmail.com>
2024-09-03 15:46:30 +0900

    ♻️refactor]: px -> rem

--- src/app/studyroomcreated/page.tsx
+++ src/app/studyroomcreated/page.tsx
@@ -7,23 +7,23 @@ import StudyCreateCard from '@/components/common/StudyCreateCard';
 export default function page() {
   return (
     <>
-      <div className="flex flex-col max-w-[375px] h-full justify-center items-center gap-[77px] px-[16px] m-auto">
-        <div className="flex flex-col gap-[20px] w-full">
-          <div className="flex justify-start items-end gap-1">
-            <h1 className="text-[24px] font-semibold">
+      <div className="flex flex-col max-w-[23.438rem] h-full justify-center items-center gap-[4.813rem] px-[1rem] m-auto">
+        <div className="flex flex-col gap-[1.25rem] w-full">
+          <div className="flex justify-start items-end gap-[0.063rem]">
+            <h1 className="text-[1.5rem] font-semibold">
               멤버들이 모두 모여
               <br />
               스터디룸이 생성되었어요
             </h1>
             <Image
-              className="pb-[4px]"
+              className="pb-[0.25rem]"
               src="/images/clapping-hands.svg"
               alt="clappingHands"
               width={24}
               height={24}
             />
           </div>
-          <h2 className="text-[14px] text-[#82829B]">
+          <h2 className="text-[0.875rem] text-[#82829B]">
             모두 함께 스터디 완주를 하는 그날까지!
           </h2>
         </div>
